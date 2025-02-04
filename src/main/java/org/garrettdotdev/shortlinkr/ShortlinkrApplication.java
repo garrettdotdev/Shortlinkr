9d97aee9f4af510e49d9e8b92792d9bc8a1a186d
Garrett Rohde <garrettrohde86@gmail.com>
2025-02-04 18:17:28 -0500

    Initial commit

---
+++ src/main/java/org/garrettdotdev/shortlinkr/ShortlinkrApplication.java
@@ -0,0 +1,13 @@
+package org.garrettdotdev.shortlinkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlinkrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortlinkrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/garrettdotdev/shortlinkr/ShortlinkrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.garrettdotdev.shortlinkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinkrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
